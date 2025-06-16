4b14ccadaabcac101e4c8d748f1fc6c94af3620f
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-06-16 03:30:32 +0200

    copying previous project

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/SpringLaMiaPizzeriaRelazioniAppliaction.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/SpringLaMiaPizzeriaRelazioniAppliaction.java
@@ -1,13 +1,12 @@
 package org.lessons.java.spring_la_mia_pizzeria_relazioni;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLaMiaPizzeriaRelazioniApplication {
+public class SpringLaMiaPizzeriaRelazioniAppliaction {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLaMiaPizzeriaRelazioniApplication.class, args);
+		SpringApplication.run(SpringLaMiaPizzeriaRelazioniAppliaction.class, args);
 	}
 
 }
