8723e46e8cfbbced3258b82d2be8b48dfea9c59b
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-06-16 02:57:43 +0200

    pom e properties

--- src/main/java/org/lessons/java/pizzeria_crud/repository/PizzasRepository.java
+++
@@ -1,9 +0,0 @@
-package org.lessons.java.pizzeria_crud.repository;
-import org.lessons.java.pizzeria_crud.model.Pizza;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PizzasRepository extends JpaRepository <Pizza, Integer> {
-    // Nuovo metodo per cercare pizze il cui nome contiene una certa stringa (case-insensitive)
-    List<Pizza> findByNameContainingIgnoreCase(String name);
-}
